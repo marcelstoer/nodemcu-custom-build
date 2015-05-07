6e3ab8a5ef051175b4afa09ff761b9ac43cde866
nodemcu-custom-build <webmaster@frightanic.com>
2015-05-07 23:09:55 +0200

    Prepare my user_modules.h for custom build

--- user_modules.h
+++ user_modules.h
@@ -1,4 +1,4 @@
-// email=fhisg@excite.com
+// email=webmaster@frightanic.com
 // branch=master
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
@@ -20,15 +20,10 @@
 #define LUA_USE_MODULES_GPIO
 #define LUA_USE_MODULES_ON
 #define LUA_USE_MODULES_NET
-#define LUA_USE_MODULES_ON
-#define LUA_USE_MODULES_SPI
 #define LUA_USE_MODULES_TMR
 #define LUA_USE_MODULES_UART
-#define LUA_USE_MODULES_OW
 #define LUA_USE_MODULES_BIT
 #define LUA_USE_MODULES_MQTT
-#define LUA_USE_MODULES_U8G
-#define LUA_USE_MODULES_WS2812
 
 #endif /* LUA_USE_MODULES */
 
