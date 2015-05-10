76070a6643b0a3b97b09203a358298e3b374dcdd
nodemcu-custom-build <alresalatbuy@gmail.com>
2015-05-10 23:04:20 +0200

    Prepare my user_modules.h for custom build

--- user_modules.h
+++ user_modules.h
@@ -1,5 +1,5 @@
-// email=charles.l.wilt@gmail.com
-// branch=dev
+// email=alresalatbuy@gmail.com
+// branch=master
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
 
@@ -18,7 +18,12 @@
 #define LUA_USE_MODULES_NODE
 #define LUA_USE_MODULES_FILE
 #define LUA_USE_MODULES_GPIO
+#define LUA_USE_MODULES_WIFI
+#define LUA_USE_MODULES_NET
+#define LUA_USE_MODULES_TMR
+#define LUA_USE_MODULES_ADC
 #define LUA_USE_MODULES_UART
+#define LUA_USE_MODULES_OW
 
 #endif /* LUA_USE_MODULES */
 
