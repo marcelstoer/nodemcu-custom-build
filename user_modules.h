354e515ee199bb5f544be114387cab4ae9d74223
nodemcu-custom-build <bascotti@gmx.de>
2015-05-11 12:33:04 +0200

    Prepare my user_modules.h for custom build

--- user_modules.h
+++ user_modules.h
@@ -1,6 +1,6 @@
-// email=htpcfreak@gmail.com
+// email=bascotti@gmx.de
 // branch=master
-// modules=node,file,gpio,wifi,net,i2c,tmr,uart,ow,mqtt,u8g,cjson
+// modules=node,file,gpio,wifi,net,tmr,uart
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
 
@@ -21,13 +21,8 @@
 #define LUA_USE_MODULES_GPIO
 #define LUA_USE_MODULES_WIFI
 #define LUA_USE_MODULES_NET
-#define LUA_USE_MODULES_I2C
 #define LUA_USE_MODULES_TMR
 #define LUA_USE_MODULES_UART
-#define LUA_USE_MODULES_OW
-#define LUA_USE_MODULES_MQTT
-#define LUA_USE_MODULES_U8G
-#define LUA_USE_MODULES_CJSON
 
 #endif /* LUA_USE_MODULES */
 
