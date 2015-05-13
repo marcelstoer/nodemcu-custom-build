7723aec3c69b98851113c8aeef2decef033fa90a
nodemcu-custom-build <vleiswal@sagateway.com>
2015-05-13 05:32:49 +0200

    Prepare my user_modules.h for custom build

--- user_modules.h
+++ user_modules.h
@@ -1,6 +1,6 @@
-// email=2010@dostips.com
+// email=vleiswal@sagateway.com
 // branch=master
-// modules=node,file,gpio,wifi,net,pwm,i2c,spi,tmr,adc,uart,ow,u8g,ws2812
+// modules=node,file,gpio,wifi,net,i2c,spi,tmr,uart,ow,bit
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
 
@@ -21,15 +21,12 @@
 #define LUA_USE_MODULES_GPIO
 #define LUA_USE_MODULES_WIFI
 #define LUA_USE_MODULES_NET
-#define LUA_USE_MODULES_PWM
 #define LUA_USE_MODULES_I2C
 #define LUA_USE_MODULES_SPI
 #define LUA_USE_MODULES_TMR
-#define LUA_USE_MODULES_ADC
 #define LUA_USE_MODULES_UART
 #define LUA_USE_MODULES_OW
-#define LUA_USE_MODULES_U8G
-#define LUA_USE_MODULES_WS2812
+#define LUA_USE_MODULES_BIT
 
 #endif /* LUA_USE_MODULES */
 
