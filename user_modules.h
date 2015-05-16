9a67f54008de2e3e14d718315f5a3f787d0e0f0d
nodemcu-custom-build <dani.eichhorn@squix.ch>
2015-05-16 10:58:17 +0200

    Prepare my user_modules.h for custom build

--- user_modules.h
+++ user_modules.h
@@ -1,6 +1,6 @@
 // email=dani.eichhorn@squix.ch
 // branch=master
-// modules=node,file,gpio,i2c,uart,bit,u8g
+// modules=node,file,gpio,i2c,tmr,uart,bit,u8g
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
 
@@ -20,6 +20,7 @@
 #define LUA_USE_MODULES_FILE
 #define LUA_USE_MODULES_GPIO
 #define LUA_USE_MODULES_I2C
+#define LUA_USE_MODULES_TMR
 #define LUA_USE_MODULES_UART
 #define LUA_USE_MODULES_BIT
 #define LUA_USE_MODULES_U8G
