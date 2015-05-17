6905b5b3222f3c39983286d459f438eddd9fb5de
nodemcu-custom-build <temus@wp.pl>
2015-05-17 11:06:31 +0200

    Prepare my user_modules.h for custom build

--- user_modules.h
+++ user_modules.h
@@ -1,6 +1,6 @@
-// email=crapthings@gmail.com
-// branch=master
-// modules=node,file,gpio,wifi,net,tmr,uart
+// email=temus@wp.pl
+// branch=dev096
+// modules=node,file,gpio,wifi,net,tmr,adc,uart,mqtt
 #ifndef __USER_MODULES_H__
 #define __USER_MODULES_H__
 
@@ -22,7 +22,9 @@
 #define LUA_USE_MODULES_WIFI
 #define LUA_USE_MODULES_NET
 #define LUA_USE_MODULES_TMR
+#define LUA_USE_MODULES_ADC
 #define LUA_USE_MODULES_UART
+#define LUA_USE_MODULES_MQTT
 
 #endif /* LUA_USE_MODULES */
 
