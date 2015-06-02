e321a369b2b000f04d95f655239fcdd9b4ba02d6
Marcel Stör <marcel@frightanic.com>
2015-06-02 22:44:46 +0200

    add Git commit ID to version string

--- set-version.sh
+++ set-version.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-sed 's/#define NODE_VERSION[^_].*/#define NODE_VERSION "NodeMCU custom build by frightanic.com\\n\\tbranch: '$X_BRANCH'\\n\\tSSL: '$X_SSL_ENABLED'\\n\\tmodules: '$X_MODULES'\\n"/g' user_version.h > user_version.h.tmp && mv user_version.h.tmp user_version.h
+X_COMMIT_ID=$(git rev-parse HEAD)
+sed 's/#define NODE_VERSION[^_].*/#define NODE_VERSION "NodeMCU custom build by frightanic.com\\n\\tbranch: '$X_BRANCH'\\n\\tcommit: '$X_COMMIT_ID'\\n\\tSSL: '$X_SSL_ENABLED'\\n\\tmodules: '$X_MODULES'\\n"/g' user_version.h > user_version.h.tmp && mv user_version.h.tmp user_version.h
 sed 's/#define BUILD_DATE.*/#define BUILD_DATE "\\tbuilt on: '"$(date "+%Y-%m-%d %H:%M")"'\\n"/g' user_version.h > user_version.h.tmp && mv user_version.h.tmp user_version.h
