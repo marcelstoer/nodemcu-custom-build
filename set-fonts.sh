8627b9026a1772ed2ec250fb60027334ed8b7217
Marcel Stör <marcel@frightanic.com>
2015-06-12 07:07:32 +0200

    Handle empty fonts string correctly

--- set-fonts.sh
+++ set-fonts.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-# replace ',' by newline and turn every item into '    U8G_FONT_TABLE_ENTRY(<font-name>) \' (except
-# the one on the last line which mustn't have the '\'
-export X_U8G_FONTS_STRING=$(echo $X_U8G_FONTS | tr , '\n' | perl -pe 'my $break = (eof) ? "" : "\\"; s/(.*)\n/    U8G_FONT_TABLE_ENTRY($1) $break\n/g')
+if [ "${X_U8G_FONTS}" == "" ]; then
+  export X_U8G_FONTS_STRING='\n'
+else
+  # replace ',' by newline and turn every item into '    U8G_FONT_TABLE_ENTRY(<font-name>) \' (except
+  # the one on the last line which mustn't have the '\'
+  export X_U8G_FONTS_STRING=$(echo $X_U8G_FONTS | tr , '\n' | perl -pe 'my $break = (eof) ? "" : "\\"; s/(.*)\n/    U8G_FONT_TABLE_ENTRY($1) $break\n/g')
+fi
 # inject the fonts string into u8g_config.h between '#define U8G_FONT_TABLE \\n' and '\n#undef U8G_FONT_TABLE_ENTRY'
 # the 's' flag in '/sg' makes . match newlines
 # Perl creates a temp file which is removed right after the manipulation
