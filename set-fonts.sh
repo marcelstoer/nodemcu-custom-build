9ff1dd096d93df66966d56301ff884ffa08581ed
Marcel Stör <marcel@frightanic.com>
2015-11-06 17:15:00 +0100

    Change regex for u8g fonts to accommodate structure in 'dev'
    fixes #12

--- set-fonts.sh
+++ set-fonts.sh
@@ -12,4 +12,4 @@ fi
 # inject the fonts string into u8g_config.h between '#define U8G_FONT_TABLE \\n' and '\n#undef U8G_FONT_TABLE_ENTRY'
 # the 's' flag in '/sg' makes . match newlines
 # Perl creates a temp file which is removed right after the manipulation
-perl -e 'local $/; $_ = <>; s/(#define U8G_FONT_TABLE \\\n)(.*)(\n#undef U8G_FONT_TABLE_ENTRY)/$1$ENV{"X_U8G_FONTS_STRING"}$3/sg; print' u8g_config.h > u8g_config.h.tmp && mv u8g_config.h.tmp u8g_config.h
+perl -e 'local $/; $_ = <>; s/(#define U8G_FONT_TABLE *\\\n)(.*)(\n#undef U8G_FONT_TABLE_ENTRY)/$1$ENV{"X_U8G_FONTS_STRING"}$3/sg; print' u8g_config.h > u8g_config.h.tmp && mv u8g_config.h.tmp u8g_config.h
