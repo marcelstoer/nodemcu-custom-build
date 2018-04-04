5cfbff95466e7c8f36b945029880eae149d9b9ea
Marcel Stör <marcelstoer@users.noreply.github.com>
2018-04-04 22:09:03 +0200

    Simplify version script

--- set-version.sh
+++ set-version.sh
@@ -3,5 +3,6 @@
 set -e
 
 X_COMMIT_ID=$(git rev-parse HEAD)
-sed 's/#define NODE_VERSION[^_].*/#define NODE_VERSION "NodeMCU custom build by frightanic.com\\n\\tbranch: '$X_BRANCH'\\n\\tcommit: '$X_COMMIT_ID'\\n\\tSSL: '$X_SSL_ENABLED'\\n\\tmodules: '$X_MODULES'\\n"/g' user_version.h > user_version.h.tmp && mv user_version.h.tmp user_version.h
-sed 's/#define BUILD_DATE.*/#define BUILD_DATE "\\tbuilt on: '"$(date "+%Y-%m-%d %H:%M")"'\\n"/g' user_version.h > user_version.h.tmp && mv user_version.h.tmp user_version.h
+BUILD_DATE="$(date "+%Y-%m-%d %H:%M")"
+sed -i 's/#define NODE_VERSION[^_].*/#define NODE_VERSION "NodeMCU custom build by frightanic.com\\n\\tbranch: '$X_BRANCH'\\n\\tcommit: '$X_COMMIT_ID'\\n\\tSSL: '$X_SSL_ENABLED'\\n\\tmodules: '$X_MODULES'\\n"/g' user_version.h
+sed -i 's/"unspecified"/"created on '"$BUILD_DATE"'\\n"/g' user_version.h
