bbfdb51aceabdcbfdca1bbcb8e8386bd3776c874
Arnim Laeuger <arnim.laeuger@gmx.net>
2018-07-23 21:42:56 +0200

    add support for u8g2

--- set-displays.sh
+++ set-displays.sh
@@ -33,3 +33,28 @@ else
   sed "/^ *UCG_DISPLAY_TABLE_ENTRY(ili.*)/d" ucg_config.h > ucg_config.h.tmp && mv ucg_config.h.tmp ucg_config.h
   sed "s/^ *UCG_DISPLAY_TABLE_ENTRY.*)/    UCG_DISPLAY_TABLE_ENTRY($X_UCG_DISPLAY_SPI)/" ucg_config.h > ucg_config.h.tmp && mv ucg_config.h.tmp ucg_config.h
 fi
+
+
+# replace default U8G2 I2C/SPI displays with the selected ones
+
+if [ "${X_U8G2_DISPLAY_I2C}" == "" ]; then
+  # one *could* delete the default entry if no display is selected...
+  :
+else
+  cat <<EOF > u8g2_displays.h.tmp
+#define U8G2_DISPLAY_TABLE_I2C_EXTRA \\
+  U8G2_DISPLAY_TABLE_ENTRY(u8g2_Setup_${X_U8G2_DISPLAY_I2C}_f, ${X_U8G2_DISPLAY_I2C})
+EOF
+  cat u8g2_displays.h >> u8g2_displays.h.tmp && mv u8g2_displays.h.tmp u8g2_displays.h
+fi
+
+if [ "${X_U8G2_DISPLAY_SPI}" == "" ]; then
+  # one *could* delete the default entry if no display is selected...
+  :
+else
+  cat <<EOF > u8g2_displays.h.tmp
+#define U8G2_DISPLAY_TABLE_SPI_EXTRA \\
+  U8G2_DISPLAY_TABLE_ENTRY(u8g2_Setup_${X_U8G2_DISPLAY_SPI}_f, ${X_U8G2_DISPLAY_SPI})
+EOF
+  cat u8g2_displays.h >> u8g2_displays.h.tmp && mv u8g2_displays.h.tmp u8g2_displays.h
+fi
