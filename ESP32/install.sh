13ec071ac3f7a70a974850c0e8d007cfae8f0362
Marcel Stör <marcelstoer@users.noreply.github.com>
2020-01-29 21:49:08 +0100

    Add ESP32 support

---
+++ ESP32/install.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+set -e
+
+echo "Running 'install' for ESP32"
+(
+  /usr/bin/python -m pip install --user -r "$PWD"/sdk/esp32-esp-idf/requirements.txt
+)
---
+++ ESP8266/install.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+set -e
+
+echo "Running 'install' for ESP8266"
+(
+  echo "Nothing to do here"
+)
