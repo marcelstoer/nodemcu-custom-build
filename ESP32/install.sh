f1e7f723e2df97c657c7a8747bccb475a053dd88
Marcel Stör <marcelstoer@users.noreply.github.com>
2021-02-07 14:56:23 +0100

    Fix Python setup for ESP32

--- ESP32/install.sh
+++ ESP32/install.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Running 'install' for ESP32"
 (
-  /usr/bin/python -m pip install setuptools
-  /usr/bin/python -m pip install --user -r "$PWD"/sdk/esp32-esp-idf/requirements.txt
+  python -m pip install setuptools
+  python -m pip install --user -r "$PWD"/sdk/esp32-esp-idf/requirements.txt
 )
